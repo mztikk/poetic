18caec5b8363ae8c73797849c0ab447fe17c54ed
Nils Feierabend <mztikk@outlook.de>
2022-01-23 20:11:42 +0100

    add: added FwdBakMerger optimizer

---
+++ src/optimizer/fwd_bak_merger.rs
@@ -0,0 +1,96 @@
+use crate::instruction::Instruction;
+
+use super::Optimize;
+
+pub(crate) struct FwdBakMerger;
+
+fn create_fwd_bak_instructions_from_total(total: i64) -> Vec<Instruction> {
+    let mut instructions = Vec::new();
+    let mut total = total;
+    if total > 0 {
+        while total > u8::MAX as i64 {
+            let fwd = Instruction::FWD(u8::MAX);
+            instructions.push(fwd);
+
+            total -= u8::MAX as i64;
+        }
+
+        if total > 0 {
+            let fwd = Instruction::FWD(total as u8);
+            instructions.push(fwd);
+        }
+    } else {
+        total = total.abs();
+
+        while total > u8::MAX as i64 {
+            let bak = Instruction::BAK(u8::MAX);
+            instructions.push(bak);
+
+            total -= u8::MAX as i64;
+        }
+
+        if total > 0 {
+            let bak = Instruction::BAK(total as u8);
+            instructions.push(bak);
+        }
+    }
+
+    instructions
+}
+
+fn get_total_count_fwd_bak(instructions: &[Instruction], start: usize) -> (i64, usize) {
+    let mut total: i64 = 0;
+    let mut j = start;
+    while j < instructions.len() {
+        match &instructions[j] {
+            Instruction::FWD(y) => {
+                total += *y as i64;
+            }
+            Instruction::BAK(y) => {
+                total -= *y as i64;
+            }
+            _ => {
+                break;
+            }
+        }
+
+        j += 1;
+    }
+
+    (total, j - start)
+}
+
+impl Optimize for FwdBakMerger {
+    fn optimize(&self, instructions: &mut [Instruction]) -> Vec<Instruction> {
+        let mut result = Vec::new();
+
+        let mut i = 0;
+        while i < instructions.len() {
+            match instructions[i] {
+                Instruction::FWD(x) => {
+                    let (mut total, count) = get_total_count_fwd_bak(instructions, i + 1);
+
+                    total += x as i64;
+
+                    result.extend_from_slice(&create_fwd_bak_instructions_from_total(total));
+
+                    i += count;
+                }
+                Instruction::BAK(x) => {
+                    let (mut total, count) = get_total_count_fwd_bak(instructions, i + 1);
+
+                    total -= x as i64;
+
+                    i += count;
+
+                    result.extend_from_slice(&create_fwd_bak_instructions_from_total(total));
+                }
+                fwd => result.push(fwd),
+            }
+
+            i += 1;
+        }
+
+        result
+    }
+}
--- src/optimizer/mod.rs
+++ src/optimizer/mod.rs
@@ -1,9 +1,13 @@
+mod fwd_bak_merger;
 mod if_eif_jmp_rewriter;
 mod inc_dec_merger;
 
 use crate::instruction::Instruction;
 
-use self::{if_eif_jmp_rewriter::IfEifJmpRewriter, inc_dec_merger::IncDecMerger};
+use self::{
+    fwd_bak_merger::FwdBakMerger, if_eif_jmp_rewriter::IfEifJmpRewriter,
+    inc_dec_merger::IncDecMerger,
+};
 
 pub trait Optimize {
     fn optimize(&self, instructions: &mut [Instruction]) -> Vec<Instruction>;
@@ -15,8 +19,11 @@ pub struct Optimizer {
 
 impl Optimizer {
     pub fn optimize(&mut self) -> Vec<Instruction> {
-        let optimizers: Vec<Box<dyn Optimize>> =
-            vec![Box::new(IncDecMerger), Box::new(IfEifJmpRewriter)];
+        let optimizers: Vec<Box<dyn Optimize>> = vec![
+            Box::new(IncDecMerger),
+            Box::new(FwdBakMerger),
+            Box::new(IfEifJmpRewriter),
+        ];
 
         for optimizer in &optimizers {
             self.instructions = optimizer.optimize(&mut self.instructions);
