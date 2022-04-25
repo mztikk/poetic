e0976d321606f96aeec6e58a1c1c8faeeb1bb050
mztikk <mztikk@outlook.de>
2022-04-25 19:56:19 +0200

    refactor parse_instructions to remove panics and return Result instead, added ParseError

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,28 @@
 use crate::instruction::Instruction;
-use std::{cmp::Ordering, collections::VecDeque};
+use std::{cmp::Ordering, collections::VecDeque, fmt::Display};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ParseError {
+    UnknownInstruction(u8),
+    NeedsArgument(u8),
+    MissingIf,
+    MissingEif,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::UnknownInstruction(instruction) => {
+                write!(f, "Unknown instruction: {}", instruction)
+            }
+            ParseError::NeedsArgument(instruction) => {
+                write!(f, "Instruction {} needs an argument", instruction)
+            }
+            ParseError::MissingIf => write!(f, "Missing IF"),
+            ParseError::MissingEif => write!(f, "Missing EIF"),
+        }
+    }
+}
 
 pub struct Parser {}
 
@@ -36,11 +59,6 @@ impl Parser {
                 }
             });
 
-        // println!(
-        //     "{}",
-        //     result.iter().map(|x| x.to_string()).collect::<String>()
-        // );
-
         result
     }
 
@@ -52,49 +70,106 @@ impl Parser {
         argument
     }
 
-    pub fn parse_instructions(intermediate: &[u8]) -> Vec<Instruction> {
+    fn check_if_eif_mismatch(instructions: &[Instruction]) -> Option<ParseError> {
+        // check for matching if/eif
+        let mut i = 0;
+        while i < instructions.len() {
+            match instructions[i] {
+                Instruction::IF => {
+                    let mut instruction_pointer = i;
+
+                    let mut nested = 1;
+                    while nested != 0 {
+                        instruction_pointer += 1;
+                        if instruction_pointer >= instructions.len() {
+                            return Some(ParseError::MissingEif);
+                        }
+
+                        let nested_instruction = instructions[instruction_pointer];
+                        match nested_instruction {
+                            Instruction::IF => {
+                                nested += 1;
+                            }
+                            Instruction::EIF => {
+                                nested -= 1;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                Instruction::EIF => {
+                    let mut instruction_pointer = i;
+
+                    let mut nested = -1;
+                    while nested != 0 {
+                        if instruction_pointer == 0 {
+                            return Some(ParseError::MissingIf);
+                        }
+
+                        instruction_pointer -= 1;
+
+                        let nested_instruction = instructions[instruction_pointer];
+                        match nested_instruction {
+                            Instruction::IF => {
+                                nested += 1;
+                            }
+                            Instruction::EIF => {
+                                nested -= 1;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            i += 1;
+        }
+
+        None
+    }
+
+    pub fn parse_instructions(intermediate: &[u8]) -> Result<Vec<Instruction>, ParseError> {
         let mut result = Vec::new();
         let mut iter = intermediate.iter();
         while let Some(arg) = iter.next() {
-            match arg {
-                0 => result.push(Instruction::END),
-                1 => result.push(Instruction::IF),
-                2 => result.push(Instruction::EIF),
-                3 => result.push(Instruction::INC(Parser::argument_conversion(
-                    iter.next()
-                        .expect("INC Instruction needs an argument")
-                        .to_owned(),
-                ))),
-                4 => result.push(Instruction::DEC(Parser::argument_conversion(
-                    iter.next()
-                        .expect("DEC Instruction needs an argument")
-                        .to_owned(),
-                ))),
-                5 => result.push(Instruction::FWD(Parser::argument_conversion(
-                    iter.next()
-                        .expect("FWD Instruction needs an argument")
-                        .to_owned(),
-                ))),
-                6 => result.push(Instruction::BAK(Parser::argument_conversion(
-                    iter.next()
-                        .expect("BAK Instruction needs an argument")
-                        .to_owned(),
-                ))),
-                7 => result.push(Instruction::OUT),
-                8 => result.push(Instruction::IN),
-                9 => result.push(Instruction::RND),
-                10 => result.push(Instruction::END),
-                _ => panic!("Unknown instruction '{}'", arg),
-            }
+            let instruction = match arg {
+                0 => Ok(Instruction::END),
+                1 => Ok(Instruction::IF),
+                2 => Ok(Instruction::EIF),
+                3 => iter
+                    .next()
+                    .map(|x| Instruction::INC(Parser::argument_conversion(*x)))
+                    .ok_or(ParseError::NeedsArgument(3)),
+                4 => iter
+                    .next()
+                    .map(|x| Instruction::DEC(Parser::argument_conversion(*x)))
+                    .ok_or(ParseError::NeedsArgument(4)),
+                5 => iter
+                    .next()
+                    .map(|x| Instruction::FWD(Parser::argument_conversion(*x)))
+                    .ok_or(ParseError::NeedsArgument(5)),
+                6 => iter
+                    .next()
+                    .map(|x| Instruction::BAK(Parser::argument_conversion(*x)))
+                    .ok_or(ParseError::NeedsArgument(6)),
+                7 => Ok(Instruction::OUT),
+                8 => Ok(Instruction::IN),
+                9 => Ok(Instruction::RND),
+                10 => Ok(Instruction::END),
+                _ => Err(ParseError::UnknownInstruction(*arg)),
+            }?;
+
+            result.push(instruction);
         }
 
-        result
+        Parser::check_if_eif_mismatch(&result).map_or(Ok(result), Err)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::instruction::Instruction;
+    use crate::{instruction::Instruction, parser::ParseError};
 
     use super::Parser;
 
@@ -164,119 +239,141 @@ mod test {
         // len 30 -> 3,0 -> 3 = INC with arg 0 should be parsed as 10
         let intermediate = Parser::parse_intermediate(&str::repeat("a", 30));
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::INC(10));
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::INC(10));
     }
 
     #[test]
-    fn test_instruction_if() {
+    fn test_instruction_if_eif() {
+        let intermediate = vec![1, 2];
+        let instructions = Parser::parse_instructions(&intermediate);
+        assert!(instructions.is_ok());
+        let instructions = instructions.unwrap();
+        assert_eq!(instructions[0], Instruction::IF);
+        assert_eq!(instructions[1], Instruction::EIF);
+    }
+
+    #[test]
+    fn test_instruction_missing_eif() {
         let intermediate = vec![1];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::IF);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::MissingEif);
     }
 
     #[test]
-    fn test_instruction_eif() {
+    fn test_instruction_missing_if() {
         let intermediate = vec![2];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::EIF);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::MissingIf);
     }
 
     #[test]
     fn test_instruction_inc() {
         let intermediate = vec![3, 1];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::INC(1));
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::INC(1));
     }
 
     #[test]
-    #[should_panic(expected = "INC Instruction needs an argument")]
     fn test_instruction_inc_needs_arg() {
-        // hide panic output
-        std::panic::set_hook(Box::new(|_| {}));
+        // INC Instruction needs an argument
         let intermediate = vec![3];
-        let _instructions = Parser::parse_instructions(&intermediate);
+        let instructions = Parser::parse_instructions(&intermediate);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::NeedsArgument(3));
     }
 
     #[test]
     fn test_instruction_dec() {
         let intermediate = vec![4, 1];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::DEC(1));
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::DEC(1));
     }
 
     #[test]
-    #[should_panic(expected = "DEC Instruction needs an argument")]
     fn test_instruction_dec_needs_arg() {
-        // hide panic output
-        std::panic::set_hook(Box::new(|_| {}));
+        // DEC Instruction needs an argument
         let intermediate = vec![4];
-        let _instructions = Parser::parse_instructions(&intermediate);
+        let instructions = Parser::parse_instructions(&intermediate);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::NeedsArgument(4));
     }
 
     #[test]
     fn test_instruction_fwd() {
         let intermediate = vec![5, 1];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::FWD(1));
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::FWD(1));
     }
 
     #[test]
-    #[should_panic(expected = "FWD Instruction needs an argument")]
     fn test_instruction_fwd_needs_arg() {
-        // hide panic output
-        std::panic::set_hook(Box::new(|_| {}));
+        // FWD Instruction needs an argument
         let intermediate = vec![5];
-        let _instructions = Parser::parse_instructions(&intermediate);
+        let instructions = Parser::parse_instructions(&intermediate);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::NeedsArgument(5));
     }
 
     #[test]
     fn test_instruction_bak() {
         let intermediate = vec![6, 1];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::BAK(1));
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::BAK(1));
     }
 
     #[test]
-    #[should_panic(expected = "BAK Instruction needs an argument")]
     fn test_instruction_bak_needs_arg() {
-        // hide panic output
-        std::panic::set_hook(Box::new(|_| {}));
+        // BAK Instruction needs an argument
         let intermediate = vec![6];
-        let _instructions = Parser::parse_instructions(&intermediate);
+        let instructions = Parser::parse_instructions(&intermediate);
+        assert!(instructions.is_err());
+        assert_eq!(instructions.unwrap_err(), ParseError::NeedsArgument(6));
     }
 
     #[test]
     fn test_instruction_out() {
         let intermediate = vec![7];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::OUT);
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::OUT);
     }
 
     #[test]
     fn test_instruction_in() {
         let intermediate = vec![8];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::IN);
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::IN);
     }
 
     #[test]
     fn test_instruction_rnd() {
         let intermediate = vec![9];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::RND);
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::RND);
     }
 
     #[test]
     fn test_instruction_end10() {
         let intermediate = vec![10];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::END);
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::END);
     }
 
     #[test]
     fn test_instruction_end0() {
         let intermediate = vec![0];
         let instructions = Parser::parse_instructions(&intermediate);
-        assert_eq!(instructions[0], Instruction::END);
+        assert!(instructions.is_ok());
+        assert_eq!(instructions.unwrap()[0], Instruction::END);
     }
 }
